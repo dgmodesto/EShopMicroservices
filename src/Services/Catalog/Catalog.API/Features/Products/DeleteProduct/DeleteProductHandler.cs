59f9404db11206371df7e1efd4976a3696942fb8
dgmodesto <douglasgomesmodesto@gmail.com>
2024-07-31 16:11:18 -0300

    feat(delete-product): implement endpoint and handle to delete a product

---
+++ src/Services/Catalog/Catalog.API/Features/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Catalog.API.Features.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+public record DeleteProductResult(bool IsSuccess);
+
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+    }
+}
+internal class DeleteProductHandler(IDocumentSession session, ILogger<DeleteProductHandler> logger) 
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductHandler.Handler called with {@Command}", command);
+
+
+        var product = await session.LoadAsync<Product>(command.Id);
+        if (product is null) {
+            throw new ProductNotFoundExceptions(command.Id);
+        }
+
+        session.Delete(product);
+        await session.SaveChangesAsync();
+
+        return new DeleteProductResult(true);
+    }
+}
