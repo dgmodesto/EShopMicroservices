0604766fd102ebd73fbd0da7dfb37d8ea5fb0d08
dgmodesto <douglasgomesmodesto@gmail.com>
2024-08-01 18:13:10 -0300

    feat(get-all-products): implement pagination to list products

--- src/Services/Catalog/Catalog.API/Features/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProducts/GetProductsHandler.cs
@@ -1,9 +1,7 @@
-﻿using Marten.Linq.QueryHandlers;
-
-namespace Catalog.API.Features.Products.GetProducts;
+﻿namespace Catalog.API.Features.Products.GetProducts;
 
 
-public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
 public record GetProductsResult(IEnumerable<Product> Products);
 
 internal class GetProductsHandler(
@@ -12,7 +10,9 @@ internal class GetProductsHandler(
 {
     public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
     {
-        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        var products = await session
+                                .Query<Product>()
+                                .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
         return new GetProductsResult(products);
     }
