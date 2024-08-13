f6d0483579c1ae433ada0a0e4b6989f2d7ff42dc
dgmodesto <douglasgomesmodesto@gmail.com>
2024-08-13 14:17:08 -0300

    feat(basket): configuring gRPC at the basket microservice to connect with discount microservice

--- src/Services/Basket/Basket.API/Features/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Features/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Features.Basket.StoreBasket;
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.Features.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) 
     : ICommand<StoreBasketResult>;
@@ -14,12 +16,28 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 
-public class StoreBasketHandler (IBasketRepository repository)
+public class StoreBasketHandler
+    (IBasketRepository repository, DiscountProtoService.DiscountProtoServiceClient discountProto)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
+        await DeductDiscount(command.Cart, cancellationToken);
+
         await repository.StoreBasket(command.Cart, cancellationToken);
+
         return new StoreBasketResult(command.Cart.UserName);
     }
+
+    public async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        // Communicate with Discount.Grpc and calculate lastest prices of products into sc
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
+    }
+
+
 }
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,13 +1,12 @@
+using Discount.Grpc.Protos;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Add Services to the Container
+//Application Services
 builder.Services.AddCarter();
-
 var assembly = typeof(Program).Assembly;
-
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
@@ -16,12 +15,12 @@ builder.Services.AddMediatR(config =>
 });
 builder.Services.AddValidatorsFromAssembly(assembly);
 
+//Data Services
 builder.Services.AddMarten(opts =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
     opts.Schema.For<ShoppingCart>().Identity(i => i.UserName);
 }).UseLightweightSessions();
-
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
@@ -37,11 +36,31 @@ builder.Services.AddStackExchangeRedisCache(options =>
     // options.InstanceName = "Basket"
 });
 
+
+//gRPC Services
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() => 
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
+});
+
+
+// Cross-Cutting Services
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("Database")!)
     .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+
 
 
 var app = builder.Build();
